cb661d5cae22484e547f15c039a71edd645c08ae
Krunal Trivedi <krunalstrivedi@gmail.com>
2023-06-21 16:40:51 +0530

    Add files via upload

---
+++ 30-5-23(Task1)/script.js
@@ -0,0 +1,10 @@
+
+function imgSlider(anything){
+    document.querySelector('.starbucks').src    = anything;
+}
+
+function changeCricleColor(color){
+    const circle = document.querySelector('.cirle');
+    circle.style.background = color;
+}
+
